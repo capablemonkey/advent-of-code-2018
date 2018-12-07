8d4a90a3efe9180be3d5870a8d3f44641f44a746
Gordon Zheng <gordon@greenhouse.io>
2018-12-07 02:11:26 -0500

    add day 7 part 2

--- day07.rb
+++ day07.rb
@@ -29,9 +29,8 @@ def can_start?(step, relationships, history)
 end
 
 def step1(relationships)
-  all_steps = relationships.map {|r| [r[:parent], r[:child]]}.flatten.uniq
-
   history = []
+  all_steps = relationships.map {|r| [r[:parent], r[:child]]}.flatten.uniq
   frontier = all_steps.
     select {|step| relationships.none? {|r| r[:child] == step}}.
     sort
@@ -47,5 +46,44 @@ def step1(relationships)
   history
 end
 
+def duration(step)
+  60 + (step.ord - 'A'.ord + 1)
+end
+
+def step2(relationships)
+  history = []
+  all_steps = relationships.map {|r| [r[:parent], r[:child]]}.flatten.uniq
+  frontier = all_steps.
+    select {|step| relationships.none? {|r| r[:child] == step}}.
+    sort
+
+  time = 0
+  workers = []
+
+  while history.size != all_steps.size
+    # find workers who have finished
+    finished_workers = workers.select {|w| w[:finishes_at] == time}
+    finished_steps = finished_workers.map {|w| w[:step] }
+    history += finished_steps
+    workers = workers - finished_workers
+
+    # calculate frontier
+    children = finished_steps.map { |step| eligible_children(step, relationships, history) }.flatten.uniq
+    children = children - workers.map {|w| w[:step]} # filter out those already being worked on
+    frontier += children
+    frontier = frontier.uniq.sort
+
+    # farm out work
+    free_workers_count = 10 - workers.size
+    steps = frontier.shift(free_workers_count)
+    workers += steps.map {|step| {:step => step, :finishes_at => time + duration(step)} }
+    frontier = frontier - workers.map {|w| w[:step]} # filter out those already being worked on
+    time += 1
+  end
+
+  time - 1
+end
+
 relationships = input_lines.map {|line| parse_line(line)}
 ap step1(relationships).join('')
+ap step2(relationships)
