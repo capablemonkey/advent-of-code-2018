892c2195d99ac7a2ffdc9c7d9932c0f52758822e
Gordon Zheng <gordon@greenhouse.io>
2018-12-08 01:29:56 -0500

    add day 8 part 2

--- day08.rb
+++ day08.rb
@@ -21,4 +21,49 @@ def part1(numbers)
   metadata.reduce(&:+)
 end
 
-puts part1(numbers)
+def value(node_id, node_list)
+  node = node_list[node_id]
+
+  return node[:metadata].reduce(&:+) if node[:children].empty?
+
+  node[:metadata].map do |child_idx|
+    if child_idx == 0
+      0
+    elsif child_idx > node[:children].size
+      0
+    else
+      child_node_id = node[:children][child_idx - 1]
+      value(child_node_id, node_list)
+    end
+  end.reduce(&:+)
+end
+
+def part2(numbers)
+  stack = [{type: :node, parent: :root}]
+  nodes = []
+
+  while !numbers.empty?
+    item = stack.pop
+    if item[:type] == :node
+      node_id = nodes.size
+      num_children, num_metadata = numbers.shift(2)
+      stack += ([{type: :metadata, parent: node_id}] * num_metadata)
+      stack += ([type: :node, parent: node_id] * num_children)
+      nodes.push({children: [], metadata: []})
+
+      unless item[:parent] == :root
+        parent = nodes[item[:parent]]
+        parent[:children].push(node_id)
+      end
+    else
+      metadata_entry = numbers.shift
+      parent = nodes[item[:parent]]
+      parent[:metadata].push(metadata_entry)
+    end
+  end
+
+  value(0, nodes)
+end
+
+puts part1(numbers.dup)
+puts part2(numbers)
