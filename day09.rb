fcdad6937d915276d8de0f5fe9a7660da40d074e
Gordon Zheng <gordon@greenhouse.io>
2018-12-09 02:39:04 -0500

    add day 9 optimization

--- day09.rb
+++ day09.rb
@@ -37,11 +37,55 @@ def highest_score(players, last_marble)
     current_player = (current_player + 1) % players
   end
 
-  scores
+  scores.max_by {|player, score| score}[1]
+end
+
+class LinkedListNode
+  attr_accessor :value, :prev, :next
+
+  def initialize(value, prev, next_)
+    @value = value
+    @prev = prev || self
+    @next = next_ || self
+  end
+
+  def insert_after(value)
+    new_node = LinkedListNode.new(value, self, @next)
+    @next.prev = new_node
+    @next = new_node
+    new_node
+  end
+
+  def delete
+    @prev.next = @next
+    @next.prev = @prev
+    @next
+  end
+end
+
+def highest_score_optimized(players, last_marble)
+  scores = Hash.new {|h,k| h[k] = 0}
+  last_marble_number = 0
+  current_marble = LinkedListNode.new(0, nil, nil)
+
+  while last_marble_number < last_marble
+    new_marble = last_marble_number += 1
+
+    if new_marble % 23 == 0
+      marble_to_remove = current_marble.prev.prev.prev.prev.prev.prev.prev
+      current_player = last_marble_number % players
+      scores[current_player] += (new_marble + marble_to_remove.value)
+      current_marble = marble_to_remove.delete
+    else
+      current_marble = current_marble.next.insert_after(new_marble)
+    end
+  end
+
+  scores.max_by {|player, score| score}[1]
 end
 
 # puts highest_score(9, 25)
 # puts highest_score(9, 100)
-# puts highest_score(10, 1618).max_by {|player, score| score}
-puts highest_score(425, 70848).max_by {|player, score| score}[1]
-puts highest_score(425, 70848 * 100).max_by {|player, score| score}[1]
+# puts highest_score(10, 1618)
+puts highest_score_optimized(425, 70848)
+puts highest_score_optimized(425, 70848 * 100)
